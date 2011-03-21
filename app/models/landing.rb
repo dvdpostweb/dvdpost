f31df70f134789043810cac2f60652f0c73879af
guillaume <gs@dvdpost.be>
2011-03-21 14:18:21 +0100

    change enum for landings

--- app/models/landing.rb
+++ app/models/landing.rb
@@ -5,7 +5,7 @@ class Landing < ActiveRecord::Base
 
   named_scope :order, lambda {|order| {:order => "ordered #{order}"}}
   named_scope :not_expirated, :conditions => 'expirated_date > now() or expirated_date is null'
-  named_scope :private, :conditions => {:login => true}
+  named_scope :private, :conditions => {:login => :private}
   named_scope :limit, lambda {|limit| {:limit => limit}}
   named_scope :by_language, lambda {|language| {:conditions => {(language == :nl ? :actif_dutch : (language == :en ? :actif_english : :actif_french)) => "YES"}}}
   named_scope :by_language_beta, lambda {|language| {:conditions => {(language == :nl ? :actif_dutch : (language == :en ? :actif_english : :actif_french)) => ["BETA","YES"]}}}
