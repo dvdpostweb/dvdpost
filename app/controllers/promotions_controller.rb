d7dffcba52924590012c31e7f1e5a47c9241f1b9
guillaume <gs@dvdpost.be>
2011-04-01 12:04:47 +0200

    poelvoorde

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,6 +1,6 @@
 class PromotionsController < ApplicationController
   def edit
-    if current_customer.credit_per_month > 2
+    if current_customer.credit_per_month > 2 && Rails.env == "production"
       redirect_to customer_path(:id => current_customer.to_param)
     end
   end
