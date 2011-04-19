2423d63efa7b1c607bd6c00aa7b18c04b8474297
guillaume <gs@dvdpost.be>
2011-04-19 17:11:54 +0200

    first touch on adult website

--- app/controllers/adult_products_controller.rb
+++ app/controllers/adult_products_controller.rb
@@ -35,6 +35,7 @@ class AdultProductsController < ApplicationController
     if params[:sort].nil?
       params[:sort] = 'normal'
     end
+    params[:adult] = true
     @countries = ProductCountry.visible.order
     respond_to do |format|
       format.html do
