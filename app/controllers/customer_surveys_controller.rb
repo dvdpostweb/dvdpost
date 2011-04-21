55eafbbbec8eb03ca669aa2f3756f5f7c5bca9d5
guillaume <gs@dvdpost.be>
2011-04-21 15:09:28 +0200

    survey plus

--- app/controllers/customer_surveys_controller.rb
+++ app/controllers/customer_surveys_controller.rb
@@ -1,6 +1,9 @@
 class CustomerSurveysController < ApplicationController
   def new
     @survey = Survey.find(params[:survey_id])
+    if @survey.customer_surveys.find_by_customer_id(current_customer.to_param)
+      redirect_to survey_path(:id =>  params[:survey_id])
+    end
     @details = @survey.survey_details
   end
 
@@ -9,13 +12,12 @@ class CustomerSurveysController < ApplicationController
     if survey.customer_surveys.find_by_customer_id(current_customer.to_param)
       redirect_to survey_path(:id =>  params[:survey_id])
     else
-      #customer_survey = CustomerSurvey.new(params[:customer_survey].merge(:customer_id => current_customer.to_param, :survey_id => survey.to_param))
-      #customer_survey.save
+      customer_survey = CustomerSurvey.new(params[:customer_survey].merge(:customer_id => current_customer.to_param, :survey_id => survey.to_param))
+      customer_survey.save
     
-      #survey.update_attribute(:total_rating, (survey.total_rating + 1))
+      survey.update_attribute(:total_rating, (survey.total_rating + 1))
     
       details = survey.survey_details.find(params[:customer_survey][:response])
-      logger.debug("@@@#{details.inspect}")
       total = (details.rating + 1)
       details.update_attribute(:rating, total)
       redirect_to survey_path(:id => params[:survey_id])
