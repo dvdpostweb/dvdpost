95ff12e89d57a63e9f6b116f13b054cdc9ba2ded
guillaume <gs@dvdpost.be>
2011-05-04 15:05:18 +0200

    adult collection

---
+++ app/models/collection.rb
@@ -0,0 +1,13 @@
+class Collection < ActiveRecord::Base
+  set_primary_key :themes_id
+  set_table_name :themes
+  
+  has_many :descriptions, :class_name => 'CollectionDescription', :foreign_key => :themes_id
+  has_and_belongs_to_many :products, :join_table => :products_to_themes, :foreign_key => :themes_id, :association_foreign_key => :products_id
+
+  named_scope :by_kind, lambda {|kind| {:conditions => {:themes_type => DVDPost.product_kinds[kind]}}}
+  named_scope :rand, :order => 'rand()'
+  def name
+    descriptions.by_language(I18n.locale).first ? descriptions.by_language(I18n.locale).first.name : "NONAME_#{to_param}"
+  end
+end
