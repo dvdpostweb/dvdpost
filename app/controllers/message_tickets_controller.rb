e6ad939f4fe96591335d82a48f217c819f4a3876
guillaume <gs@dvdpost.be>
2011-05-06 11:11:19 +0200

    new variable in mail

--- app/controllers/message_tickets_controller.rb
+++ app/controllers/message_tickets_controller.rb
@@ -2,7 +2,7 @@ class MessageTicketsController < ApplicationController
   def create
     ticket = current_customer.tickets.find(params[:ticket_id])
     if ticket
-      @message = MessageTicket.new(:ticket => ticket, :mail_id => DVDPost.email[:message_free], :data => "$$$body$$$:::#{params[:message]};;;")
+      @message = MessageTicket.new(:ticket => ticket, :mail_id => DVDPost.email[:message_free], :data => "$$$content$$$:::#{params[:message]};;;")
       @message.save
     end
     redirect_to message_path(:id => params[:ticket_id])
