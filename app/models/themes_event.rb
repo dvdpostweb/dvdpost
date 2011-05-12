29b249b4b29fa853005cde48edcbd2f9636fb207
guillaume <gs@dvdpost.be>
2011-05-12 16:53:42 +0200

    theme event

--- app/models/themes_event.rb
+++ app/models/themes_event.rb
@@ -1,6 +1,6 @@
 class ThemesEvent < ActiveRecord::Base
   db_magic :slaves => [ :slave01, :slave02 ] if ENV['APP'] == "1"
 
-  named_scope :selected, :conditions => {:selected => true}
+  named_scope :selected, :conditions => {:selected => 1}
   named_scope :selected_beta, :conditions => {:selected => 2}
 end
