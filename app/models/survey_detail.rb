25334e17054bfc8a10de94f3c1c135d72c757a47
guillaume <gs@dvdpost.be>
2011-05-12 10:43:39 +0200

    survey change order

--- app/models/survey_detail.rb
+++ app/models/survey_detail.rb
@@ -3,9 +3,11 @@ class SurveyDetail < ActiveRecord::Base
   belongs_to :product, :primary_key => :products_id, :foreign_key => :reference_id
   belongs_to :actor, :primary_key => :actors_id, :foreign_key => :reference_id
   belongs_to :director, :primary_key => :directors_id, :foreign_key => :reference_id
-  
+
+  named_scope :ordered, :order => "rating desc"
+
   def image(survey_id)
      File.join(DVDPost.images_path, 'surveys', survey_id.to_s, "images", "#{id}.jpg")
   end
-  
+
 end
