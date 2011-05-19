48e224df1e90b3f53347d5c56ec91d8866180642
guillaume <gs@dvdpost.be>
2011-05-19 17:45:14 +0200

    choose only adult studio

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -3,17 +3,17 @@ class StudiosController < ApplicationController
     if !params[:letter]
       @studios = OrderedHash.new()
       @studios_count = OrderedHash.new()
-      @studios_count.push(0, Studio.by_number.count)
-      @studios.push(0, Studio.by_number.limit(10))
+      @studios_count.push(0, Studio.by_kind(params[:kind]).by_number.count)
+      @studios.push(0, Studio.by_kind(params[:kind]).by_number.limit(10))
       ('a'..'z').each do |l|
-        @studios_count.push(l, Studio.by_letter(l).count)
-        @studios.push(l, Studio.by_letter(l).limit(10))
+        @studios_count.push(l, Studio.by_kind(params[:kind]).by_letter(l).count)
+        @studios.push(l, Studio.by_kind(params[:kind]).by_letter(l).limit(10))
       end
     else
       if params[:letter] == "0"
-        @studios = Studio.by_number
+        @studios = Studio.by_kind(params[:kind]).by_number
       else
-        @studios = Studio.by_letter(params[:letter])
+        @studios = Studio.by_kind(params[:kind]).by_letter(params[:letter])
       end
     end
   end
