0d9860cc66bfc52dcde40f329b0aba8648efdf55
guillaume <gs@dvdpost.be>
2011-05-20 15:41:04 +0200

    design add adult

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -4,16 +4,16 @@ class StudiosController < ApplicationController
       @studios = OrderedHash.new()
       @studios_count = OrderedHash.new()
       @studios_count.push(0, Studio.by_kind(params[:kind]).by_number.count)
-      @studios.push(0, Studio.by_kind(params[:kind]).by_number.limit(10))
+      @studios.push(0, Studio.by_kind(params[:kind]).by_number.limit(10).ordered)
       ('a'..'z').each do |l|
         @studios_count.push(l, Studio.by_kind(params[:kind]).by_letter(l).count)
-        @studios.push(l, Studio.by_kind(params[:kind]).by_letter(l).limit(10))
+        @studios.push(l, Studio.by_kind(params[:kind]).by_letter(l).limit(10).ordered)
       end
     else
       if params[:letter] == "0"
-        @studios = Studio.by_kind(params[:kind]).by_number
+        @studios = Studio.by_kind(params[:kind]).by_number.ordered
       else
-        @studios = Studio.by_kind(params[:kind]).by_letter(params[:letter])
+        @studios = Studio.by_kind(params[:kind]).by_letter(params[:letter]).ordered
       end
     end
   end
