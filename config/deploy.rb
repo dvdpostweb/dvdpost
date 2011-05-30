29d34d524271f28334200c41a9351b7e8352ae16
guillaume <gs@dvdpost.be>
2011-05-30 16:57:26 +0200

    gemfile clean

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,50 +17,50 @@ end
 # Thinking Sphinx
 namespace :thinking_sphinx do
   task :configure, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
   end
   task :index, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
   end
   task :reindex, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:reindex RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:reindex RAILS_ENV=#{rails_env}"
   end
   task :start, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
   end
   task :stop, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
   end
   task :restart, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
   end
   task :rebuild, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
   end
 end
 
 # Thinking Sphinx typing shortcuts
 namespace :ts do
   task :conf, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
   end
   task :in, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
   end
   task :rein, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:reindex RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:reindex RAILS_ENV=#{rails_env}"
   end
   task :start, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
   end
   task :stop, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
   end
   task :restart, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
   end
   task :rebuild, :roles => [:app] do
-    run "cd #{current_path}; rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
+    run "cd #{current_path};bundle exec rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
   end
 end
 
