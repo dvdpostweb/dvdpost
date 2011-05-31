e139b0553b5b6382194d82f275b2e7eb99e51793
guillaume <gs@dvdpost.be>
2011-05-31 11:40:18 +0200

    change system of mouseover picture

--- public/javascripts/actors.js
+++ public/javascripts/actors.js
@@ -7,13 +7,12 @@ $(function() {
     id = parseInt(link.replace(reg,''),10)
     letter = link.replace(reg_a,'')
     var reg_jpg = new RegExp(/\/[\d]*_[\d]\.jpg/);
-    var reg_link = new RegExp(/\/[\d]*\//);
     image_id = "#thumb_"+letter
     src = $(image_id).attr('src')
     new_src = src.replace(reg_jpg, "/"+id+"_1.jpg");
     $(image_id).attr('src',new_src)
-    actor_href = $("#thumb_link_"+letter).attr('href')
-    new_href =actor_href.replace(reg_link, "/"+id+"/");
+    
+    new_href =$(this).attr('href');
     if ($('#search-performers').hasClass('letter'))
     {
       if ($(this).hasClass('picture')){
