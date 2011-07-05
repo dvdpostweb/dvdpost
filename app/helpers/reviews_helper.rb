51c322ef221d3ad4c558ce89181f72ea3b5dcd24
guillaume <gs@dvdpost.be>
2011-07-05 11:30:34 +0200

    new review system

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,12 +2,12 @@ module ReviewsHelper
   def new_position(product_rate) 
     if product_rate.position.nil?
       "<span class='stat new'>#{image_tag( 'new.png', :alt =>'new')} NEW</span>"
-    elsif product_rate.position < 0
-      "<span class='stat down'>#{image_tag( 'down.png', :alt =>'down')} #{product_rate.position}</span>"
     elsif product_rate.position == 0
-      "<span class='stat no-change'>#{image_tag( 'no-change.png', :alt =>'not change')} ="
+      "<span class='stat no-change'>#{image_tag( 'no-change.png', :alt =>'not change')} =</span>"
     elsif product_rate.position > 0
-      "<span class='stat up'>#{image_tag( 'up.png', :alt =>'up')} +#{product_rate.position}"
+      "<span class='stat up'>#{image_tag( 'up.png', :alt =>'up')} +#{product_rate.position}</span>"
+    else
+      "<span class='stat down'>#{image_tag( 'down.png', :alt =>'down')} #{product_rate.position}</span>"
     end
   end
 end
