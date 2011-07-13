37ee5954393ced4a5b99a80933bc2a38c5f87e23
guillaume <gs@dvdpost.be>
2011-07-13 10:10:58 +0200

    change theme and add serie

--- app/models/themes_event.rb
+++ app/models/themes_event.rb
@@ -1,8 +1,9 @@
 class ThemesEvent < ActiveRecord::Base
   db_magic :slaves => [ :slave01, :slave02 ] if ENV['APP'] == "1"
 
-  named_scope :selected, :conditions => {:selected => 1}
-  named_scope :selected_beta, :conditions => {:selected => 2}
+  named_scope :selected, :conditions => {:themes_events_selection_id => 1}
+  named_scope :selected_beta, :conditions => {:themes_events_selection_id => 2}
   named_scope :by_kind, lambda {|kind| {:conditions => {:kind => kind.to_s}}}
+  named_scope :old, :conditions => {:themes_events_selection_id => 3}
   
 end
