c2c1cff99c6cd8d0f73dba78e81d98a6836b0fbf
guillaume <gs@dvdpost.be>
2011-07-13 10:08:31 +0200

    change rule for serie and migration

--- db/migrate/20110712113315_theme_events_selections.rb
+++ db/migrate/20110712113315_theme_events_selections.rb
@@ -4,11 +4,11 @@ class ThemeEventsSelections < ActiveRecord::Migration
       t.string :name
       t.timestamps
     end
-    rename_column :themes_events, :selected, :themes_events_selection_id
+    add_column :themes_events, :themes_events_selection_id, :integer
   end
 
   def self.down
     drop_table :themes_events_selections
-    rename_column :themes_events, :themes_events_selection_id, :selected
+    remove_column :themes_events, :themes_events_selection_id
   end
 end
