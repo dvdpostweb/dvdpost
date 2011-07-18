12ac80717f7e2e24755a8e4774446b4ad7e60e26
guillaume <gs@dvdpost.be>
2011-07-18 12:40:57 +0200

    add more

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -218,4 +218,10 @@ $(function() {
       });
     });
   }
+  $('.review_more').live('click',function(){
+    $(this).parent().parent().children('.full_review').show();
+    $(this).parent().parent().children('.short_review').hide();
+    $(this).hide();
+    return false; 
+  })
 });
