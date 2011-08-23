75175372b1f95273fe1b4d282df30d55081edb2e
guillaume <gs@dvdpost.be>
2011-08-23 11:37:28 +0200

    fix theme beta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,7 +61,11 @@ class ApplicationController < ActionController::Base
 
   def theme_actif_hp
     if Rails.env == "pre_production"
-      return  ThemesEvent.selected_beta.hp.by_kind(params[:kind]).last
+      theme_hp = ThemesEvent.selected_beta.hp.by_kind(params[:kind]).last
+      unless theme_hp
+        theme_hp = ThemesEvent.selected.hp.by_kind(params[:kind]).last
+      end  
+      return theme_hp
     else
       return ThemesEvent.selected.hp.by_kind(params[:kind]).last
     end
