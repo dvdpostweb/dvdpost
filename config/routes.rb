cfc503554bc2b639d26722bc4e291b73ea916f70
guillaume <gs@dvdpost.be>
2011-10-17 16:35:22 +0200

    remove top + vod adult highlight

--- config/routes.rb
+++ config/routes.rb
@@ -94,9 +94,7 @@ ActionController::Routing::Routes.draw do |map|
     localized.resources :contests, :only => [:new, :create, :index]
     localized.resources :quizzes, :only => [:show, :index]
     
-    localized.menu_tops 'menu_tops', :controller => :products, :action => :menu_tops, :conditions => {:method => :get}
     localized.validation 'validation', :controller => :home, :action => :validation, :conditions => {:method => :get}
-    localized.menu_categories 'menu_categories', :controller => :products, :action => :menu_categories, :conditions => {:method => :get}
     
 
     localized.resources :wishlist_items, :only => [:new, :create, :update, :destroy]
