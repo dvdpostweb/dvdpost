b96b08347e3a162c2152bae79afc4918824716be
guillaume <gs@dvdpost.be>
2012-03-06 17:08:29 +0100

    add

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -110,10 +110,14 @@ class ShoppingCartsController < ApplicationController
   end
   def init_data
     cart = current_customer.shopping_carts.ordered
-    @cart_count = cart.count
     @cart = cart.paginate(:per_page => 3, :page => 1)
-    @shipping = ShoppingCart.shipping(@cart_count)
-    @price = current_customer.shopping_carts.sum(:quantity)
+    
+    @count = current_customer.shopping_carts.sum(:quantity)
+    @shipping = ShoppingCart.shipping(@count)
+    @price = 0
+    current_customer.shopping_carts.each do |c|
+      @price += c.quantity * c.product.price_sale
+    end
     @price += @shipping
   end
 end
