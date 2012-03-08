967e24871208140043922c12b79803969ba4dc23
guillaume <gs@dvdpost.be>
2012-03-08 11:40:46 +0100

    review character

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -42,7 +42,7 @@ class ReviewsController < ApplicationController
   def create
     begin
       @product = Product.both_available.find(params[:product_id])
-      params[:review][:text] = params[:review][:text].gsub(/\n/, '')
+      params[:review][:text] = params[:review][:text].gsub(/\n/, '').gsub(//, "'")
       review = @product.reviews.build(params[:review])
       review.customer = current_customer
       review.languages_id = DVDPost.product_languages[I18n.locale]
