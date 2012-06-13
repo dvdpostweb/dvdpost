b6bc831b5c4094a7882d0194148ddbe7069f6736
guillaume <gs@dvdpost.be>
2012-06-13 11:09:38 +0200

    padding

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -82,31 +82,31 @@ namespace :deploy do
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
-  
   after "deploy:restart" do
-   env_config = <<-EOF
-   # Settings specified here will take precedence over those in config/environment.rb
+    env_config = <<-EOF
+    # Settings specified here will take precedence over those in config/environment.rb
 
-   # In the development environment your application's code is reloaded on
-   # every request.  This slows down response time but is perfect for development
-   # since you don't have to restart the webserver when you make code changes.
-   config.cache_classes = false
+    # In the development environment your application's code is reloaded on
+    # every request.  This slows down response time but is perfect for development
+    # since you don't have to restart the webserver when you make code changes.
+    config.cache_classes = false
 
-   # Log error messages when you accidentally call methods on nil.
-   config.whiny_nils = true
+    # Log error messages when you accidentally call methods on nil.
+    config.whiny_nils = true
 
-   # Show full error reports and disable caching
-   config.action_controller.consider_all_requests_local = true
-   config.action_view.debug_rjs                         = true
-   config.action_controller.perform_caching             = true
-   #config.cache_store = :file_store, RAILS_ROOT + "/tmp/cache"
-   config.cache_store = :mem_cache_store, '192.168.100.206:11211'
-   # Don't care if the mailer can't send
-   config.action_mailer.raise_delivery_errors = false
-   ENV['APP'] = "1"
+    # Show full error reports and disable caching
+    config.action_controller.consider_all_requests_local = true
+    config.action_view.debug_rjs                         = true
+    config.action_controller.perform_caching             = true
+    #config.cache_store = :file_store, RAILS_ROOT + "/tmp/cache"
+    config.cache_store = :mem_cache_store, '192.168.100.206:11211'
+    # Don't care if the mailer can't send
+    config.action_mailer.raise_delivery_errors = false
+    ENV['APP'] = "1"
     EOF
-   put env_config, "#{current_path}/config/environments/staging.rb"
+    put env_config, "#{current_path}/config/environments/staging.rb"
+
   end
 end
-before 'deploy:symlink', 'deploy:stop_ts'
-after 'deploy:symlink', 'deploy:update_ts'
+before 'deploy:create_symlink', 'deploy:stop_ts'
+after 'deploy:create_symlink', 'deploy:update_ts'
