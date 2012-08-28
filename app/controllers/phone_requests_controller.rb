f08d6e86f66cde7b8ab4294fdced4f0d79bd733a
guillaume <gs@dvdpost.be>
2012-08-28 10:22:30 +0200

    phone request public change

--- app/controllers/phone_requests_controller.rb
+++ app/controllers/phone_requests_controller.rb
@@ -1,10 +1,10 @@
 class PhoneRequestsController < ApplicationController
   def new
-    @phone_request = ENV['HOST_OK'] == "0" ? PhoneRequest.new : PhoneRequestPublic.new
+    @phone_request = PhoneRequest.new
   end
 
   def create
-    @phone_request = ENV['HOST_OK'] == "0" ? PhoneRequest.new(params[:phone_request].merge(:customer_id => current_customer.to_param)) : PhoneRequestPublic.new(params[:phone_request])
+    @phone_request = PhoneRequest.new(params[:phone_request].merge(:customer_id => current_customer ? current_customer.to_param : 0 )) 
     if @phone_request.save
       flash[:notice] = t('messages.index.messages.phone_request_send_successfully')
       redirect_to ENV['HOST_OK'] == "0" ? messages_path : root_path
