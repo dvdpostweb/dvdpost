b345cf2854332ed16e99e321617cac6c0abce2f2
guillaume <gs@dvdpost.be>
2012-09-05 13:47:36 +0200

    test mobile fu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ require 'db_charmer'
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include ApplicationHelper
+  has_mobile_fu
 
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
     helper_method :current_customer, :unless => :is_it_xml?
@@ -237,7 +238,7 @@ class ApplicationController < ActionController::Base
 
     def redirect_to_mobile_if_applicable
       @browser = Browser.new(:ua => request.user_agent, :accept_language => "en-us")
-      unless mobile_request? || cookies[:prefer_full_site] || !@browser.mobile? || browser.iphone? || browser.ipad?
+      unless mobile_request? || cookies[:prefer_full_site] || !@browser.mobile? || browser.iphone? || browser.ipad? || browsertablet?
         redirect_to request.protocol + "m." + request.host_with_port + request.request_uri and return
       end
     end
