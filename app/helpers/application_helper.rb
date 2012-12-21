4bdae2b765bd8d956f3ab521482aa3263d7f9d28
guillaume <gs@dvdpost.be>
2012-12-21 16:32:13 +0100

    step member choice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,6 +64,8 @@ module ApplicationHelper
         if params[:controller] != 'shops' && params[:controller] != 'shopping_carts' && params[:controller] != 'shopping_orders' && !(params[:controller] == 'info' && params[:page_name] == 'buy') && !(params[:controller] == 'info' && params[:page_name] == 'withdrawal_period') && params[:controller] != 'phone_requests' && params[:action] != 'validation'
           redirect_to shop_path(:locale => params[:locale], :kind => :normal) and return
         end
+      elsif current_customer.customers_registration_step.to_i == 90
+        redirect_to php_path('step_member_choice.php')
       elsif current_customer.customers_registration_step.to_i != 100  && current_customer.customers_registration_step.to_i != 95
         redirect_to php_path
       end
