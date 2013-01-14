0abbb850c45d405d963eb15108682a31f50429f5
guillaume <gs@dvdpost.be>
2013-01-14 15:30:43 +0100

    wishlist update

---
+++ public/javascripts/wishlist_mobile.js
@@ -0,0 +1,20 @@
+$(function() {
+  var options_change_priority = {
+	};
+  $(".wishlist_item_priority").live("click", function() {
+    url = $(this).parent().parent().attr('action')
+    console.log(url)
+    value = $(this).val()
+    var regex = new RegExp(".*/wishlist_items/([0-9]*).*");
+    res = regex.exec(url)
+    
+    loader = 'ajax-loader.gif';
+    $(this).parent().parent().ajaxSubmit(options_change_priority);
+    html_item = $(this).parent().parent()
+    parent = $(this).parent().parent()
+    content = html_item.html();
+    $(this).parent().parent().html("<div style='height:17px; text-align:center' class='load'><img src='/images/"+loader+"'/></div>")
+    return false; // prevent default behaviour
+  });
+  
+});
