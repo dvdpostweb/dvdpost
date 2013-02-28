19aadc29e57f05678c6b5c10eecab507b9c33704
guillaume <gs@dvdpost.be>
2013-02-28 15:33:50 +0100

    browser 1.6

--- app/helpers/streaming_products_helper.rb
+++ app/helpers/streaming_products_helper.rb
@@ -21,6 +21,7 @@ module StreamingProductsHelper
         $("#player").html("<object width='696' height='389'><param name='movie' value='http://#{DVDPost.streaming_url}/StrobeMediaPlayback.swf'></param><param name='flashvars' value='src=http://#{DVDPost.streaming_url}/#{token_name}_#{streaming.languages.by_language(:fr).first.short_alpha}_#{streaming.subtitles.count > 0 ? streaming.subtitles.by_language(:fr).first.short_alpha : 'non'}.f4m&loop=false&autoPlay=true&streamType=recorded&verbose=true&initialBufferTime=5&expandedBufferTime=30'></param><param name='allowFullScreen' value='true'></param><param name='allowscriptaccess' value='always'></param><embed src='http://#{DVDPost.streaming_url}/StrobeMediaPlayback.swf' type='application/x-shockwave-flash' allowscriptaccess='always' allowfullscreen='true' width='696' height='389' flashvars='src=http://#{DVDPost.streaming_url}/#{token_name}_#{streaming.languages.by_language(:fr).first.short_alpha}_#{streaming.subtitles.count > 0 ? streaming.subtitles.by_language(:fr).first.short_alpha : 'non'}.f4m&loop=false&autoPlay=true&streamType=recorded&verbose=true&initialBufferTime=5&expandedBufferTime=30'></embed></object>")
         script
       end
+      Rails.logger.debug { "@@@#{script}" }
     if mobile_request?
       script
     else
