0dd34801d565787243f5f38546c67bb79e1c3eac
guillaume <gs@dvdpost.be>
2013-03-11 14:26:07 +0100

    open uri add

--- config/initializers/dvdpost_config.rb
+++ config/initializers/dvdpost_config.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 require 'dvdpost_constants' # Require here so we do not have to do it in the code
 require 'prefix' # Require here so we do not have to do it in the code
 DVDPostConfig = HashWithIndifferentAccess.new.merge(YAML.load_file("#{RAILS_ROOT}/config/dvdpost_config.yml").symbolize_keys)
