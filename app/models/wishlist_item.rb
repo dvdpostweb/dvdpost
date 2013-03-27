e02a47c2f1965c166d2249769ec8a6261bf13b34
guillaume <gs@dvdpost.be>
2013-03-27 12:01:35 +0100

    debug ppv add some source

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -33,20 +33,38 @@ class WishlistItem < ActiveRecord::Base
   def self.wishlist_source(params, wishlist_source)
     if params[:view_mode] == 'recommended'
       source = wishlist_source[:recommendation]
-    elsif params[:sort] == 'most_viewed' && params[:limit].to_i == 20
+    elsif params[:ppv] == "1"
+      source = wishlist_source[:ppv]
+    elsif params[:sort] == 'most_viewed' && params[:limit].to_i == 40
       source = wishlist_source[:most_viewed]
-    elsif params[:sort] == 'rating' && params[:limit].to_i == 50
+    elsif params[:sort] == 'token_month'
+      source = wishlist_source[:most_viewed_vod]
+    elsif params[:highlight_best_vod] == '1'
+      source = wishlist_source[:prefered_vod]
+    elsif params[:highlight_best] == '1'
       source = wishlist_source[:prefered]
-    elsif params[:view_mode] == 'recent'
+    elsif params[:view_mode] == 'production_year_all'
       source = wishlist_source[:new]
+    elsif params[:view_mode] == 'production_year_vod'
+      source = wishlist_source[:new]
+    elsif params[:view_mode] == 'vod_recent'
+      source = wishlist_source[:last_added_vod]
+    elsif params[:view_mode] == 'recent'
+      source = wishlist_source[:last_added]
     elsif params[:view_mode] == 'soon'
       source = wishlist_source[:soon]
+    elsif params[:view_mode] == 'vod_soon'
+      source = wishlist_source[:soon_vod]
     elsif params[:view_mode] == 'cinema'
       source = wishlist_source[:cinema]
     elsif params[:search]
       source = wishlist_source[:search]
     elsif params[:category_id]
-      source = wishlist_source[:category]
+      if params[:filter] == 'vod'
+        source = wishlist_source[:category]
+      else
+        source = wishlist_source[:category_vod]
+      end
     elsif params[:actor_id]
       source = wishlist_source[:actor]
     elsif params[:director_id]
