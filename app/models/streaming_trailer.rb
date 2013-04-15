772fae9c397e9d33643bc0fc5fd2a6cfeb6a55c0
guillaume <gs@dvdpost.be>
2013-04-15 11:49:49 +0200

    trailers streaming

--- app/models/streaming_trailer.rb
+++ app/models/streaming_trailer.rb
@@ -2,7 +2,7 @@ class StreamingTrailer < ActiveRecord::Base
   has_many :subtitles, :foreign_key => :undertitles_id, :primary_key => :subtitle_id
   has_many :languages, :foreign_key => :languages_id, :primary_key => :language_id
   named_scope :available, lambda {{:conditions => ['available = ? and status = "online_test_ok"', 1]}}
-  named_scope :available_beta, lambda {{:conditions => ['available = ? and status != "deleted"', 1]}}
+  named_scope :available_beta, lambda {{:conditions => ['status != "deleted"', 1]}}
   named_scope :prefered_audio, lambda {|language_id| {:conditions => {:language_id => language_id }}}
   named_scope :sub_nil, lambda {|language_id| {:conditions => {:subtitle_id => nil }}}
   
@@ -10,7 +10,7 @@ class StreamingTrailer < ActiveRecord::Base
   named_scope :not_prefered, lambda {|language_id| {:conditions => ["language_id != :language_id and (subtitle_id != :language_id or subtitle_id is null)",{:language_id => language_id}]}}
   
   def self.get_best_version(imdb_id, local)
-    if Rails.env != "pre_production"
+    if Rails.env == "production"
       streaming = available.prefered_audio(DVDPost.customer_languages[local]).sub_nil.find_by_imdb_id(imdb_id)
       if streaming.nil?
         streaming = available.prefered_audio(DVDPost.customer_languages[local]).find_by_imdb_id(imdb_id)
@@ -28,5 +28,13 @@ class StreamingTrailer < ActiveRecord::Base
       streaming = available_beta.first
     end
     streaming
+  end
+
+  def is_new?
+    true
+  end
+
+  def url
+    false
   end
 end
