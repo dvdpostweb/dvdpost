68ed472f56e97e6688b97614c91574624b84016b
guillaume <gs@dvdpost.be>
2013-04-24 16:20:05 +0200

    svod

--- app/controllers/customers_svods_controller.rb
+++ app/controllers/customers_svods_controller.rb
@@ -1,4 +1,29 @@
 class CustomersSvodsController < ApplicationController
   def show
+    
+  end
+
+  def update
+    case current_customer.svod_adult
+      when 0
+        current_customer.abo_history(Subscription.action[:svod_adult_on], 130129)
+        svod = CustomersSvod.find_or_create_by_customer_id(:customer_id => current_customer.id, :validity_period => 'M', :amount => 19.95, :product_abo_id => 130129)
+        svod.update_attribute(:validityto, 1.month.from_now)
+        current_customer.update_attribute(:svod_adult, 1)
+      when 1
+        current_customer.abo_history(Subscription.action[:svod_adult_off], 130129)
+        current_customer.update_attribute(:svod_adult, 3)
+      when 2
+        current_customer.abo_history(Subscription.action[:svod_adult_off], 130129)
+        current_customer.update_attribute(:svod_adult, 4)
+      when 3
+        current_customer.abo_history(Subscription.action[:svod_adult_break_off], 130129)
+        current_customer.update_attribute(:svod_adult, 1)
+      when 4
+        current_customer.abo_history(Subscription.action[:svod_adult_break_off], 130129)
+        current_customer.update_attribute(:svod_adult, 2)
+    end
+    flash[:notice] = 'done'
+    redirect_to root_path
   end
 end
