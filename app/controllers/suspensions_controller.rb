45684d6b3c4e014c28ba216f5ef2cab0e830c83c
guillaume <gs@dvdpost.be>
2013-05-08 15:34:50 +0200

    suspension debug

--- app/controllers/suspensions_controller.rb
+++ app/controllers/suspensions_controller.rb
@@ -18,7 +18,7 @@ class SuspensionsController < ApplicationController
 
   def create
     #begin
-      path = "http://www.dvdpost.com/#{DVDPost.url_suspension}?language=#{I18n.locale}";
+      path = "http://www.dvdpost.be/#{DVDPost.url_suspension}?language=#{I18n.locale}";
       if !current_customer.suspended? && suspension_count_current_year < 3
        duration = params[:suspensions][:duration].to_i
         Rails.logger.debug { "@@@#{current_customer.to_param} , #{duration}, #{path}" }
