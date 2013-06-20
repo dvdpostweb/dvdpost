aba3896a3bc3f614d3f1170134934ffcd458286b
tiguss <gs@dvdpost.be>
2013-06-20 16:07:38 +0200

    unsub

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,8 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = true
-#config.cache_store = :file_store, RAILS_ROOT + "/tmp/cache"
-config.cache_store = :mem_cache_store, '192.168.100.206:11211'
+config.cache_store = :file_store, RAILS_ROOT + "/tmp/cache"
+#config.cache_store = :mem_cache_store, '192.168.100.206:11211'
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 ENV['APP'] = "2"
