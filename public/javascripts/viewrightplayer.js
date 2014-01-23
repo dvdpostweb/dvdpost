8f6c4caadd1df70dda31d8b78dfd66f2682d9d2f
guillaume <gs@dvdpost.be>
2014-01-23 17:15:05 +0100

    change play back

--- public/javascripts/viewrightplayer.js
+++ public/javascripts/viewrightplayer.js
@@ -986,7 +986,7 @@ window.ViewRightPlayer = (function() {
 
 		if (currentPosition !== this._lastPosition) {
 			this.onPositionChanged(currentPosition, this._lastPosition);
-			$('#playback-indicator > DIV > DIV').width(player.getPlaybackPercentage() + '%');
+			$('#playback-indicator > DIV > DIV').width(this.getPlaybackPercentage() + '%');
 		}
 
 		this._lastPosition = currentPosition;
