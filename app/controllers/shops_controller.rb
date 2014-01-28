70b95603cb8ded7f5e8ff9e112426b445f478b1e
guillaume <gs@dvdpost.be>
2014-01-28 17:16:12 +0100

    moodmovies

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,15 +1,20 @@
 class ShopsController < ApplicationController
   def show
-    list_base = Rails.env == "production" ? ProductList.shop.status : ProductList.shop
-    list = list_base.by_language(DVDPost.product_languages[I18n.locale]).first
-    @items = list ? list.products : nil
-    cart = current_customer.shopping_carts.ordered
-    @cart_count = cart.sum(:quantity)
-    @cart = cart.paginate(:per_page => 3, :page => 1)
-    price_data = ShoppingCart.price(current_customer)
-    @total = price_data[:total]
-    @shipping = price_data[:shipping]
-    @reduce = price_data[:reduce]
-    @price_reduced = price_data[:price_reduced]
+    if current_customer && current_customer.address.belgian?
+      list_base = Rails.env == "production" ? ProductList.shop.status : ProductList.shop
+      list = list_base.by_language(DVDPost.product_languages[I18n.locale]).first
+      @items = list ? list.products : nil
+      cart = current_customer.shopping_carts.ordered
+      @cart_count = cart.sum(:quantity)
+      @cart = cart.paginate(:per_page => 3, :page => 1)
+      price_data = ShoppingCart.price(current_customer)
+      @total = price_data[:total]
+      @shipping = price_data[:shipping]
+      @reduce = price_data[:reduce]
+      @price_reduced = price_data[:price_reduced]
+    else
+      flash[:error] = t('shops.show.only_belgian_customer')
+      redirect_to root_path
+    end
   end
 end
