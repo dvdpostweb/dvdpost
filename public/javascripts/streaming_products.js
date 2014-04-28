5f06ce3944a1a9d098b7683822d11b30d6d3a24c
guillaume <gs@dvdpost.be>
2014-04-28 16:56:15 +0200

    cdebug

--- public/javascripts/streaming_products.js
+++ public/javascripts/streaming_products.js
@@ -70,10 +70,6 @@ $(function() {
   
   $('.qualityvod').live("click", function() {
     url = $(this).attr('href')
-    var regex = new RegExp(".*/products/([0-9]*).*");
-    res = regex.exec(url)
-    product_vod_id = $('#product_id').html()
-    send_event('Movie', 'PlayStart', product_vod_id,'')
     response_id = getParameterByName('response_id')
     content = $('#presentation').html()
     loader = 'loading.gif';
