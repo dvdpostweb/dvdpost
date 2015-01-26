7bbd9f2600a6a643dc010e1020ef7becb188ac5d
guillaume <it@dvdpost.be>
2015-01-26 14:14:58 +0100

    change sso link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -114,7 +114,7 @@ module ApplicationHelper
 
   def oauth_client
     params = OAUTH.clone
-    params[:site] = 'https://sso.dvdpost.nl' if nederlands?
+    #params[:site] = 'https://sso.dvdpost.be' if nederlands?
     client_id = params.delete(:client_id)
     client_secret = params.delete(:client_secret)
     @client ||= OAuth2::Client.new(
