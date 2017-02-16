531f82c9e79714c740da3dc643251b7b99da6dd3
popac <web.guru.serbia@gmail.com>
2017-02-16 17:13:28 +0100

    Set deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -27,10 +27,10 @@ set :user, "dvdpostapp"
 set :domain,  "217.112.190.50"
 set :port, 23051
 
-set :domain2, "217.112.190.50:32051"
+#set :domain2, "217.112.190.50:32051"
 ##set :port, 23051
 
-#server '217.112.190.50', port: 23051, user: 'dvdpostapp', roles: %w{web app}
+server '217.112.190.50', port: 23051, user: 'dvdpostapp', roles: %w{web app}
 #server '217.112.190.50', port: 23051, user: 'dvdpostapp', roles: %w{web app}
 
 role :web,  domain
@@ -95,13 +95,13 @@ namespace :deploy do
     EOF
     put db_config, "#{release_path}/config/database.yml"
   end
-  
+
   # Restart passenger on deploy
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => {:no_release => true} do
     run "touch #{current_path}/tmp/restart.txt"
   end
-  
+
   [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
@@ -146,12 +146,12 @@ after "deploy:restart" do
 
 end
 
-   
-#namespace :deploy do  
-#  desc "Update the crontab file"  
-#  task :update_crontab, :roles => :db do  
-#    run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"  
-#  end  
+
+#namespace :deploy do
+#  desc "Update the crontab file"
+#  task :update_crontab, :roles => :db do
+#    run "cd #{release_path} && bundle exec whenever --update-crontab #{application}"
+#  end
 #end
 
 before 'deploy:create_symlink', 'deploy:stop_ts'
