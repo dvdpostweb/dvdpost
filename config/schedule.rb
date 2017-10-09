df0af3b324f54f46fcb745ce29ea11e23c7d6e3b
YOUR NAME <YOUR@EMAIL.com>
2017-10-09 12:36:12 +0200

    set reindex

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,34 +20,35 @@
 # Learn more: http://github.com/javan/whenever
 set :output, './log/cron.log'
 
-every :reboot do  
-  rake "thinking_sphinx:start"  
-end
- 
-every 1.day, :at => '11:59 am' do 
-  rake "thinking_sphinx:reindex"  
-end
-every 1.day, :at => '03:00 am' do  
+every :reboot do
+  rake "thinking_sphinx:start"
   rake "thinking_sphinx:reindex"  
 end
 
-every 1.day, :at => '2:30 pm' do 
+every 1.day, :at => '11:59 am' do
+  rake "thinking_sphinx:reindex"
+end
+every 1.day, :at => '03:00 am' do
+  rake "thinking_sphinx:reindex"
+end
+
+every 1.day, :at => '2:30 pm' do
  rake "rake friendly_id:make_slugs MODEL=Director ORDER=directors_id SLUG=1"
 end
-every 1.day, :at => '2:35 pm' do 
+every 1.day, :at => '2:35 pm' do
  rake "rake friendly_id:make_slugs MODEL=Actor ORDER=actors_id SLUG=1"
 end
-every 1.day, :at => '2:40 pm' do 
+every 1.day, :at => '2:40 pm' do
  rake "rake friendly_id:make_slugs MODEL=ThemesEvent SLUG=1"
 end
 
-every 1.day, :at => '1:00 pm' do 
+every 1.day, :at => '1:00 pm' do
   runner "Product.rating_first"
 end
-every 1.day, :at => '1:10 pm' do 
+every 1.day, :at => '1:10 pm' do
   runner "ProductDescription.seo"
 end
 
-every 1.day, :at => '02:00 am' do  
+every 1.day, :at => '02:00 am' do
   command  "cd /home/webapps/dvdpostapp/pre_production/current && RAILS_ENV=pre_production bundle exec rake thinking_sphinx:reindex --silent :output"
 end
