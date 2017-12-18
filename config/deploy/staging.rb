0cccec1ed55f76929f65637cf3583145cdb9508d
popac <aleksandar.popovic@codepop.biz>
2017-12-19 00:17:51 +0100

    Enable SSH forwarding

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -32,8 +32,8 @@ role :db, domain, :primary => true
 
 set :scm, :git
 set :branch, "master"
-set :scm_user, 'dvdpost'
-set :scm_passphrase, "[y'|\E7U158]9*"
+#set :scm_user, 'dvdpost'
+#set :scm_passphrase, "[y'|\E7U158]9*"
 set :repository, "git@github.com:dvdpost/dvdpost.git"
 set :deploy_via, :remote_cache
 
